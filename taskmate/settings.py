ec80caa80c742eb3ebc3611ebf2a5570889e0215
hiscloan <thorin.pickard@gmail.com>
2025-04-16 11:21:48 +0200

    White Noise

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'white_noise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
